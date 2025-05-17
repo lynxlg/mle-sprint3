c86be5185d2de6db47c3e9f41c90458a498144ae
Eydis Artrald <lynx.lg@gmail.com>
2025-05-17 16:42:33 +0000

    1

---
+++ app/main.py
@@ -0,0 +1,22 @@
+# импортируем библиотеку для работы со случайными числами
+import random
+
+# импортируем класс для создания экземпляра FastAPI-приложения
+from fastapi import FastAPI
+
+# создаём экземпляр FastAPI-приложения
+app = FastAPI()
+
+# обрабатываем запросы к корню приложения
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# обрабатываем запросы к специальному пути для получения предсказания модели
+# временно имитируем предсказание со случайной генерацией score
+@app.get("/api/churn/{user_id}")
+def get_prediction_for_item(user_id: str):
+    return {"user_id": user_id, "score": random.random()} 
+@app.get("/api/credit/{client_id}")
+def is_credit_approved(client_id: str):
+    return {"approved":1 if random.random()>0.8 else 0}
